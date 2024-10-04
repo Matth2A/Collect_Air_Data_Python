be89b359448805cb389094cd73c667ad9fc14eb3
Matth2A <matthis.pinheirocruz@gmail.com>
2024-10-04 12:44:35 +0300

    UPD: requirement dowload simplification

--- AQICN/aqicn.py
+++ AQICN/aqicn.py
@@ -5,11 +5,13 @@ from Packages.CreateFolder import createFolder
 if __name__ == "__main__":
 
     # List of links to download data
-    docLink = ["2015H1", "2016H1", "2017H1", "2018H1",
-    "2019Q1", "2019Q2", "2019Q3", "2019Q4",
-    "2020Q1", "2020Q2", "2020Q3", "2020Q4",
-    "2021Q1", "2021Q2", "2021Q3", "2021Q4",
-    "2022Q1", "2022Q2", "2022Q3", "2022Q4"]
+    docLink = [
+        "2015H1", "2016H1", "2017H1", "2018H1",
+        "2019Q1", "2019Q2", "2019Q3", "2019Q4",
+        "2020Q1", "2020Q2", "2020Q3", "2020Q4",
+        "2021Q1", "2021Q2", "2021Q3", "2021Q4",
+        "2022Q1", "2022Q2", "2022Q3", "2022Q4"
+            ]
 
     # Loop through the list of links
     for link in docLink:
@@ -23,4 +25,5 @@ if __name__ == "__main__":
         # Save the data
         with open(".//aqicnData//"+link, "wb") as file:
             file.write(data)
-            break
+        # only one for testing
+        break
